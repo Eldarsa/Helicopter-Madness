c8ad2eceda9d13cd2d62af7cf5978e1675f05e7b
Elmeister <eldar.sandanger@gmail.com>
2021-01-25 14:22:21 +0100

    first commit

---
+++ android/src/com/mygdx/helicopter_madness/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.helicopter_madness;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.helicopter_madness.HelicopterMadness;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new HelicopterMadness(), config);
+	}
+}
---
+++ desktop/src/com/mygdx/helicopter_madness/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.helicopter_madness.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.helicopter_madness.HelicopterMadness;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HelicopterMadness(), config);
+	}
+}
